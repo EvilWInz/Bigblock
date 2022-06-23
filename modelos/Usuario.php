034ed8fb39b23c032e72bc0bea79221d3717374a
EvilWInz <sergionicolasgalindocante@gmail.com>
2022-06-23 16:46:56 -0500

    New form delet name.html:;,

--- modelos/Usuario.php
+++ modelos/Usuario.php
@@ -15,7 +15,7 @@ class Usuario{
     public function login(){
         $datos=new ConexionBd();
         $mysql=$datos->conectar();
-        $consulta = "SELECT password FROM Usuario where nombreUsuario = '$this->nombreUsuario'";
+        $consulta = "SELECT password FROM usuario where nombreUsuario = '$this->nombreUsuario'";
         $resultado=$mysql->query($consulta);
         
         if( $resultado = mysqli_fetch_array($resultado)){
@@ -26,12 +26,8 @@ class Usuario{
                 
         }else{
              return false;
-            
             }
                 
-        }else{
-           return false;
-
         }
         
 
